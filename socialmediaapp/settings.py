40ba0c868aa86a9ee5fd90d4de2fdcad10b4a368
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2023-01-10 11:58:43 +0100

    Setup project for deployment

--- socialmediaapp/settings.py
+++ socialmediaapp/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+
 load_dotenv()
 
 
@@ -84,10 +86,11 @@ WSGI_APPLICATION = 'socialmediaapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": dj_database_url.parse(os.getenv("DATABASE_URL"), conn_max_age=600),
 }
 
 
